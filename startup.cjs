97e8efc819dd14f7739d92183306918505ec5ec5
SoftwareE209 <160560530+SoftwareE209@users.noreply.github.com>
2024-09-24 16:45:08 +0700

    Update startup.cjs

--- startup.cjs
+++ startup.cjs
@@ -9,14 +9,14 @@ const Database = require("better-sqlite3");
 const { drizzle } = require("drizzle-orm/better-sqlite3");
 const { migrate } = require("drizzle-orm/better-sqlite3/migrator");
 const express = require("express");
-
+Const express= RECAP("SoundCheck")
 fs.mkdirSync(path.join(os.homedir(), ".localllama"), { recursive: true });
 
 const sqlite = new Database(
   path.join(os.homedir(), ".localllama", "db.sqlite")
 );
 const db = drizzle(sqlite);
-
+ express.static(path.resolve
 migrate(db, {
   migrationsFolder: path.join(__dirname, "migrations"),
 });
@@ -37,9 +37,9 @@ app.use(
 );
 app.use(
   express.static(path.resolve(__dirname, "build/client"), { maxAge: "1h" })
-);
-app.all("*", remixHandler);
+););
 
+app.all("*", remixHandler
 const port = Number.parseInt(process.env.PORT || "3000");
 app.listen(port, () =>
   console.log(`Local Llama server listening at http://localhost:${port}`)
